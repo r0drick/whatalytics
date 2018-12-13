f97bb08a0d03d5071d7cf49d278bb6f5f6bd43dc
Rodrigo Addor <rodrigotladdor@gmail.com>
2018-12-13 19:44:55 -0200

    File uploading

--- src/App.js
+++ src/App.js
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+const fileHandler = file => {
+  const reader = new FileReader()
+  reader.onloadend = e => {
+    if (e.target.readyState === FileReader.DONE)
+      console.log(e.target.result)
   }
+  reader.readAsText(file)
 }
 
-export default App;
+export default class App extends React.Component {
+  render() {
+    return (
+        <>
+          <span className='logo'>Whatalytics</span>
+          <input type={'file'} accept={'.txt'} onChange={e => fileHandler(e.target.files[0])}/>
+        </>
+    )
+  }
+}
