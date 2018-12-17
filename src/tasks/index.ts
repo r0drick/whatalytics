f9fc89349da92279ec4f2cdaccf6389884b3dd01
Rodrigo Addor <rodrigotladdor@gmail.com>
2018-12-17 17:35:31 -0200

    Added MessagesPerUser and WordsPerUser tasks

--- src/tasks/index.ts
+++ src/tasks/index.ts
@@ -1,6 +1,11 @@
 import Message from '../utils/Message'
+import MessagesPerUser from './MessagesPerUser'
+import WordsPerUser from './WordsPerUser'
 
-export const getTasks = (): Task[] => []
+export const getTasks = (): Task[] => [
+		new MessagesPerUser(),
+		new WordsPerUser()
+]
 
 export default interface Task {
 	readonly name: string
